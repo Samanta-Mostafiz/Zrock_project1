c53ca86707fb0d55c3b249e8ea30f2ddc3d619dc
Mostafiz-riya <rimostafiz99@gmail.com>
2024-06-26 14:03:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// var type = new Typed(".auto", {
+//     strings: ["Samanta Mostafiz"],
+//     typeSpeed: 200,
+//     backSpeed:200,
+//     loop: false, 
+//     onComplete: function(self) {
+//         self.cursor.remove(); // Remove the cursor after completion
+//     }
+// });
