99a4b5fb21e0ecaafe3f6e2cd89ede18e58f94e2
Mostafiz-riya <rimostafiz99@gmail.com>
2024-06-27 02:00:25 +0600

    git commit -m"final"

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// var type = new Typed(".auto", {
-//     strings: ["Samanta Mostafiz"],
-//     typeSpeed: 200,
-//     backSpeed:200,
-//     loop: false, 
-//     onComplete: function(self) {
-//         self.cursor.remove(); // Remove the cursor after completion
-//     }
-// });
+const typed= new Typed(".multi",{
+    strings:["Frontend Web Developer","UI/UX designer"],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+
+})
